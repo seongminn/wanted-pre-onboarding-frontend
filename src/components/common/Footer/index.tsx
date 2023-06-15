738fc77c3772e3f7d92e9d74b580f4fbff0fceee
seongminn <berry9901@gmail.com>
2023-06-15 16:26:12 +0900

    feat: Footer 컴포넌트 생성

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,12 @@
+import './style.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      원티드 프리온보딩 프론트엔드 - 선발 과제 <br />
+      <p className="footer__desc">© 2023 SEONGMINN, All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
