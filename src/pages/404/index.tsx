aac3a3a6efc5b9a52ea59564afa6c450ae2ad7ec
seongminn <berry9901@gmail.com>
2023-06-21 02:47:12 +0900

    fix: 404 페이지 생성 및 배포 환경에서의 리다이렉트 처리

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,5 +1,23 @@
+import './style.scss';
+
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@/components/common/Button';
+import { PATH } from '@/constants/path';
+
 const NotFoundPage = () => {
-  return <div>페이지가 없습니다.</div>;
+  const navigate = useNavigate();
+  const handleButton = () => {
+    navigate(PATH.MAIN);
+  };
+
+  return (
+    <div className="inner-wrapper">
+      <b className="log">404</b>
+      <p className="description">없는 페이지입니다!</p>
+      <Button onClick={handleButton}>홈으로 돌아가기</Button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
